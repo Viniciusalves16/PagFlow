aab7767f050caba9eadb81466601ba586c0eff68
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-05-27 17:44:13 -0300

    configurar fábrica de contêineres de ouvintes Kafka com concorrência e reconhecimento de registro

--- src/main/java/com/example/PagFlow/config/ConsumerConfigKafka.java
+++ src/main/java/com/example/PagFlow/config/ConsumerConfigKafka.java
@@ -1,6 +1,7 @@
 package com.example.PagFlow.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.apache.kafka.common.serialization.StringDeserializer; // Importe este
@@ -9,10 +10,12 @@ import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @EnableKafka
 @Configuration
 public class ConsumerConfigKafka {
+    // This class configures the Kafka consumer for the PagFlow application.
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -27,4 +30,14 @@ public class ConsumerConfigKafka {
                 new StringDeserializer()
         );
     }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+        return factory;
+    }
+
 }
